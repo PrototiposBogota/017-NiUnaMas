872ae0814275a34f9ec6ac4e3162b3e8ef9a0a18
Herbert V <herbertvargas@gmail.com>
2018-10-11 23:19:09 -0500

    add icons to admin view

--- backend/n1mapp/config/routes.rb
+++ backend/n1mapp/config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#landing'
+  
+  #root 'users#sign_in'
 end
